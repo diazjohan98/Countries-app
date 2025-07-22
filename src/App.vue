f90f6c1729b2056c6166cbb61c87767e782adf1a
diazjohan98 <vasquez.johan@correounivalle.edu.co>
2025-07-22 09:04:20 -0500

    Initialize countries-app with Vue.js, TypeScript and Tailwind CSS

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import PageHeader from './components/PageHeader.vue';
+</script>
+
+<template>
+    <PageHeader />    <!-- Other components or content can be added here -->
+</template>
---
+++ src/components/PageHeader.vue
@@ -0,0 +1,7 @@
+
+<template>
+    <div class="text-center py-24 bg-white shadow-md rounded-lg">
+        <h1 class="text-4xl font-bold tracking-widest text-sky-700">Countries App</h1>
+        <h6 class="mt-4 text-xl">A simple app to search for countries</h6>
+    </div>
+</template>
