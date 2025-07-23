a7531897bd321b1fb22acd84902449d2b21ff20b
diazjohan98 <vasquez.johan@correounivalle.edu.co>
2025-07-22 21:37:02 -0500

    feat: added axios, consumed RestCountries API, designed layout with Tailwind, and created CountryItem and CountryList components

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,30 @@
 <script setup lang="ts">
-import PageHeader from './components/PageHeader.vue';
+import { onMounted, ref } from "vue";
+
+import PageHeader from "./components/PageHeader.vue";
+import CountryList from "./components/CountryList.vue";
+import axiosClient from "./utils/axios";
+import { Country } from "./models/country.model";
+
+const countries = ref<Country[]>([]);
+
+const fetchCounries = async () => {
+  try {
+    const { data } = await axiosClient.get("/all?fields=name,flags,population,region,capital");
+    countries.value = data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+onMounted(() => {
+  fetchCounries();
+});
 </script>
 
 <template>
-    <PageHeader />    <!-- Other components or content can be added here -->
+  <PageHeader />
+  <div class="container max-w.screen-lg mx-auto px-6">
+    <CountryList :countries="countries" />
+  </div>
 </template>
